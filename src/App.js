c40c30efe2317d037202e93a5d7eced4310ffcd5
Denny29 <43240668+Denny29@users.noreply.github.com>
2022-05-16 17:51:55 -0400

    Player Turn done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import "./styles.css";
+import React, { useState } from "react";
 
 export default function App() {
+  let [playerTurn, setPlayerTurn] = useState("Red");
+
   return (
     <div className="App">
-      <div className="blue">
-        <div className="slot"></div>
+      <div className="board">
+        <div
+          className={"slot " + playerTurn}
+          onClick={() => togglePlayerTurn(playerTurn, setPlayerTurn)}
+        ></div>
       </div>
+
+      <h1>
+        It's <span id={playerTurn}> {playerTurn}'s </span> turn
+      </h1>
     </div>
   );
+}
+
+function togglePlayerTurn(turn, setTurn) {
+  if (turn === "Red") {
+    setTurn("Yellow");
+  } else {
+    setTurn("Red");
+  }
 }
