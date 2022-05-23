b0e955c664611297d4195bd7486ce20a09fa91d6
Denny29 <43240668+Denny29@users.noreply.github.com>
2022-05-23 18:06:46 -0400

    started column drop

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import "./styles.css";
 import React, { useState } from "react";
 
 export default function App() {
-  let [playerTurn, setPlayerTurn] = useState("Red");
+  let [playerTurn, setPlayerTurn] = useState();
+  let board = ["", "", "", "", "", ""];
+  let [tokens, changeTokens] = useState(board);
 
   return (
     <div className="App">
       <div className="board">
         <div
-          className={"slot " + playerTurn}
+          className="column"
           onClick={() => togglePlayerTurn(playerTurn, setPlayerTurn)}
-        ></div>
+        >
+          <Slot color={tokens[0]} loc={0} />
+          <Slot color={tokens[1]} />
+          <Slot color={tokens[2]} />
+          <Slot color={tokens[3]} />
+          <Slot color={tokens[4]} />
+          <Slot color={tokens[5]} />
+        </div>
       </div>
 
       <h1>
@@ -19,6 +28,19 @@ export default function App() {
     </div>
   );
 }
+
+function Slot(props) {
+  return (
+    <div
+      className={"slot " + props.color}
+      onClick={() => addToken(props.color, props.loc)}
+    >
+      {" "}
+    </div>
+  );
+}
+
+function addToken(board, loc) {}
 
 function togglePlayerTurn(turn, setTurn) {
   if (turn === "Red") {
