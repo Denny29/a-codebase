d2c220301a86ca6288ab04e8f345953f3310acf9
Denny29 <43240668+Denny29@users.noreply.github.com>
2022-06-06 17:40:40 -0400

    Sorta made the tokens work a little buggy.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import React, { useState } from "react";
 
 export default function App() {
   let [playerTurn, setPlayerTurn] = useState();
-  let board = ["", "", "", "", "", ""];
-  let [tokens, changeTokens] = useState(board);
+  let [tokens, changeTokens] = useState(["", "", "", "", "", ""]);
 
   return (
     <div className="App">
@@ -13,12 +12,48 @@ export default function App() {
           className="column"
           onClick={() => togglePlayerTurn(playerTurn, setPlayerTurn)}
         >
-          <Slot color={tokens[0]} loc={0} />
-          <Slot color={tokens[1]} />
-          <Slot color={tokens[2]} />
-          <Slot color={tokens[3]} />
-          <Slot color={tokens[4]} />
-          <Slot color={tokens[5]} />
+          <Slot
+            color={tokens[0]}
+            turn={playerTurn}
+            update={changeTokens}
+            board={tokens}
+            loc={0}
+          />
+          <Slot
+            color={tokens[1]}
+            turn={playerTurn}
+            update={changeTokens}
+            board={tokens}
+            loc={1}
+          />
+          <Slot
+            color={tokens[2]}
+            turn={playerTurn}
+            update={changeTokens}
+            board={tokens}
+            loc={2}
+          />
+          <Slot
+            color={tokens[3]}
+            turn={playerTurn}
+            update={changeTokens}
+            board={tokens}
+            loc={3}
+          />
+          <Slot
+            color={tokens[4]}
+            turn={playerTurn}
+            update={changeTokens}
+            board={tokens}
+            loc={4}
+          />
+          <Slot
+            color={tokens[5]}
+            turn={playerTurn}
+            update={changeTokens}
+            board={tokens}
+            loc={5}
+          />
         </div>
       </div>
 
@@ -33,14 +68,18 @@ function Slot(props) {
   return (
     <div
       className={"slot " + props.color}
-      onClick={() => addToken(props.color, props.loc)}
+      onClick={() => addToken(props.turn, props.update, props.board, props.loc)}
     >
       {" "}
     </div>
   );
 }
 
-function addToken(board, loc) {}
+function addToken(turn, update, board, loc) {
+  board[loc] = turn;
+  console.log(board);
+  update(board);
+}
 
 function togglePlayerTurn(turn, setTurn) {
   if (turn === "Red") {
